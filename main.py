6386370272beb42033574a572fcc84e79d40e257
liva <ebammo20@gmail.com>
2023-02-03 20:57:18 +0330

    Simple Decorators

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
 
-Returning Functions From Functions
-Python also allows you to use functions as return values.
-The following example returns one of the inner functions from the outer parent() function
+Simple Decorators
+Now that you’ve seen that functions are just like any other object in Python, you’re ready to move
+on and see the magical beast that is the Python decorator.
 
-The somewhat cryptic output simply means that the first variable refers to the local first_child() function inside of
-parent(), while second points to second_child().
-
-You can now use first and second as if they are regular functions, even though the functions they point to can’t be
-accessed directly Finally, note that in the earlier example you executed the inner functions within the parent
-function, for instance first_child(). However, in this last example, you did not add parentheses to the inner
-functions—first_child—upon returning. That way, you got a reference to each function that you could call in the
-future. Make sense?
+In effect, the name say_whee now points to the wrapper() inner function. Remember that you return wrapper as a
+function when you call my_decorator(say_whee) However, wrapper() has a reference to the original say_whee() as func,
+and calls that function between the two calls to print().
 """
 
 
-def parent(num):
-    def first_child():
-        return "Hi, I am Emma"
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
 
-    def second_child():
-        return "Call me Liam"
+    return wrapper
 
-    if num == 1:
-        return first_child
-    else:
-        return second_child
 
+def say_whee():
+    print("Whee!")
+
+
+say_whee = my_decorator(say_whee)
 
 if __name__ == "__main__":
-    first = parent(1)
-    second = parent(2)
-    print(first)
-    print(second)
-
+    say_whee()
+    print(say_whee)
