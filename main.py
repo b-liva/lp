c03b2216a891eaf5da152572013032ad3589f404
liva <ebammo20@gmail.com>
2023-02-03 21:01:39 +0330

    Syntactic Sugar! @my_decorator

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
 
-Simple Decorators
-Now that you’ve seen that functions are just like any other object in Python, you’re ready to move
-on and see the magical beast that is the Python decorator.
-
-In effect, the name say_whee now points to the wrapper() inner function. Remember that you return wrapper as a
-function when you call my_decorator(say_whee) However, wrapper() has a reference to the original say_whee() as func,
-and calls that function between the two calls to print().
-
-Put simply: decorators wrap a function, modifying its behavior.
-
-Before moving on, let’s have a look at a second example. Because wrapper() is a regular Python function,
-the way a decorator modifies a function can change dynamically. So as not to disturb your neighbors, the following
-example will only run the decorated code during the day
-If you try to call say_whee() after bedtime, nothing will happen.
-"""
-from datetime import datetime
+Syntactic Sugar!
+So, @my_decorator is just an easier way of saying say_whee = my_decorator(say_whee). It’s how you
+apply a decorator to a function. """
 
 
-def not_during_the_night(func):
+def my_decorator(func):
     def wrapper():
-        if 7 <= datetime.now().hour < 22:
-            func()
-        else:
-            pass  # Hush, the neighbors are asleep
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
 
     return wrapper
 
 
+@my_decorator
 def say_whee():
     print("Whee!")
 
 
-say_whee = not_during_the_night(say_whee)
+# say_whee = my_decorator(say_whee)
 
 if __name__ == "__main__":
     say_whee()
