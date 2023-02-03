bf5cdaa06469206ef5ab6468bf2b7c0c3ce697c8
liva <ebammo20@gmail.com>
2023-02-03 21:23:18 +0330

    Returning Values From Decorated Functions  without return

--- decorators.py
+++ decorators.py
@@ -2,4 +2,5 @@ def do_twice(func):
     def wrapper_do_twice(*args, **kwargs):
         func(*args, **kwargs)
         func(*args, **kwargs)
+
     return wrapper_do_twice
--- main.py
+++ main.py
@@ -1,34 +1,25 @@
+from decorators import do_twice
+
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
 
-Decorating Functions With Arguments
+Returning Values From Decorated Functions 
+What happens to the return value of decorated functions? Well, that’s up to
+the decorator to decide. Let’s say you decorate a simple function as follows
 
-Say that you have a function that accepts some arguments. Can you still decorate
-it? The problem is that the inner function wrapper_do_twice() does not take any arguments, but name="World" was
-passed to it. You could fix this by letting wrapper_do_twice() accept one argument, but then it would not work for
-the say_whee() function you created earlier.
-
-The solution is to use *args and **kwargs in the inner wrapper function. Then it will accept an arbitrary number of
-positional and keyword arguments. Rewrite decorators.py as follows: The wrapper_do_twice() inner function now accepts
-any number of arguments and passes them on to the function it decorates. Now both your say_whee() and greet()
-examples works """
-from decorators import do_twice
+"""
 
 
 @do_twice
-def say_whee():
-    print('Whee!')
-
-
-@do_twice
-def greet(name):
-    print(f"Hello {name}")
+def return_greeting(name):
+    print("Creating greeting")
+    return f"Hi {name}"
 
 
 # say_whee = my_decorator(say_whee)
 
 if __name__ == "__main__":
-    say_whee()
-    print(say_whee)
-    greet("world")
-    print(greet)
+    hi_adam = return_greeting("Adam")
+    """Because the do_twice_wrapper() doesn’t explicitly return a value, the call return_greeting("Adam") ended up 
+    returning None. """
+    print(hi_adam)  # Returns None. It only prints
