cd1aca081b7dd5e02a3024037fbe514bb4a6d7b8
liva <ebammo20@gmail.com>
2023-02-03 20:48:07 +0330

    Inner Functions

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 """
-First-Class Objects
-In Python, functions are first-class objects. This means that functions can be passed around and used as arguments,
- just like any other object (string, int, float, list, and so on). Consider the following three functions:
+Thanks to: https://realpython.com/primer-on-python-decorators/
+
+Inner Functions
+It’s possible to define functions inside other functions.
+Such functions are called inner functions. Here’s an example of a function with two inner functions
+Note that the order in which the inner functions are defined does not matter. Like with any other functions, the printing only happens when the inner functions are executed.
+
+Furthermore, the inner functions are not defined until the parent function is called.
+They are locally scoped to parent(): they only exist inside the parent() function as local variables.
+Try calling first_child(). You should get an error
+Whenever you call parent(), the inner functions first_child() and second_child() are also called. But because of their local scope, they aren’t available outside of the parent() function.
 """
 
 
-def say_hello(name):
-    return f"Hello {name}"
+def parent():
+    print("Printing from the parent() function")
 
+    def first_child():
+        print("Printing from the first_child() function")
 
-def be_awesome(name):
-    return f"Yo {name}, together we are the awesomest!"
+    def second_child():
+        print("Printing from the second_child() function")
 
-
-def greet_bob(greeter_func):
-    return greeter_func("Bob")
+    second_child()
+    first_child()
 
 
 if __name__ == "__main__":
-    print(say_hello('Robin'))
-    print(be_awesome('Jack'))
-    print(greet_bob(say_hello))
-    print(greet_bob(be_awesome))
+    parent()
+    first_child()
