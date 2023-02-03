564f6a5984b26da7d3d6b4178bcc2801d1c7c05e
liva <ebammo20@gmail.com>
2023-02-03 22:20:55 +0330

    Debugging Code : more useful example

--- decorator/decorator_main.py
+++ decorator/decorator_main.py
@@ -1,22 +1,20 @@
+import math
 from decorators import debug
 
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
 
 Debugging Code 
-The following @debug decorator will print the arguments a function is called with as well as its 
-return value every time the function is called """
+More useful info.
+ """
+
+# Apply a decorator to a standard library function
+math.factorial = debug(math.factorial)
 
 
-@debug
-def make_greeting(name, age=None):
-    if age is None:
-        return f"Howdy {name}!"
-    else:
-        return f"Whoa {name}! {age} already, you are growing up!"
+def approximate_e(terms=18):
+    return sum(1 / math.factorial(n) for n in range(terms))
 
 
 if __name__ == "__main__":
-    make_greeting("Benjamin")
-    make_greeting("Richard", age=112)
-    make_greeting(name="Dorrisile", age=116)
+    approximate_e(5)
