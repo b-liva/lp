4b4dfec9df3f1ac5029330d95ea7e5dbd2788282
liva <ebammo20@gmail.com>
2023-02-03 22:46:46 +0330

    Registering Plugins

--- decorator/decorator_main.py
+++ decorator/decorator_main.py
@@ -1,19 +1,42 @@
-from decorators import slow_down
+import random
+
+from decorators import register, PLUGINS
 
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
-Slowing Down Code: example
- """
+
+Registering Plugins 
+Decorators don’t have to wrap the function they’re decorating. They can also simply register that 
+a function exists and return it unwrapped. This can be used, for instance, to create a light-weight plug-in 
+architecture 
+
+The @register decorator simply stores a reference to the decorated function in the global PLUGINS dict. Note that you 
+do not have to write an inner function or use @functools.wraps in this example because you are returning the original 
+function unmodified. 
+
+The randomly_greet() function randomly chooses one of the registered functions to use. Note that the PLUGINS 
+dictionary already contains references to each function object that is registered as a plugin Using the @register 
+decorator, you can create your own curated list of interesting variables, effectively hand-picking some functions 
+from globals() """
 
 
-@slow_down
-def countdown(from_number):
-    if from_number < 1:
-        print("Liftoff!")
-    else:
-        print(from_number)
-        countdown(from_number - 1)
+@register
+def say_hello(name):
+    return f"Hello {name}"
+
+
+@register
+def be_awesome(name):
+    return f"Yo {name}, together we are the awesomest!"
+
+
+def randomly_greet(name):
+    greeter, greeter_func = random.choice(list(PLUGINS.items()))
+    print(f"Using {greeter!r}")
+    return greeter_func(name)
 
 
 if __name__ == "__main__":
-    countdown(3)
+    print(PLUGINS)
+    randomly_greet('Alice')
+    print(globals())
--- decorator/decorators.py
+++ decorator/decorators.py
@@ -1,15 +1,7 @@
-import functools
-import time
+PLUGINS = dict()
 
 
-def slow_down(func):
-    """Sleep 1 second before calling the function"""
-    """The @slow_down decorator always sleeps for one second. Later, you’ll see how to control the rate by passing an 
-    argument to the decorator """
-
-    @functools.wraps(func)
-    def wrapper_slow_down(*args, **kwargs):
-        time.sleep(1)
-        return func(*args, **kwargs)
-
-    return wrapper_slow_down
+def register(func):
+    """Register a function as a plug-in"""
+    PLUGINS[func.__name__] = func
+    return func
