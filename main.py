d14b698ef6ea353b9ccf03025b87dd1a6c0bd311
liva <ebammo20@gmail.com>
2023-02-03 20:43:04 +0330

    First-Class Objects

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+"""
+First-Class Objects
+In Python, functions are first-class objects. This means that functions can be passed around and used as arguments,
+ just like any other object (string, int, float, list, and so on). Consider the following three functions:
+"""
+
+
+def say_hello(name):
+    return f"Hello {name}"
+
+
+def be_awesome(name):
+    return f"Yo {name}, together we are the awesomest!"
+
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
 
 
 if __name__ == "__main__":
-    print('main...')
-    
+    print(say_hello('Robin'))
+    print(be_awesome('Jack'))
+    print(greet_bob(say_hello))
+    print(greet_bob(be_awesome))
