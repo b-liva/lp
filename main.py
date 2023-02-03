b402c18827781ff42f5e5114751fcc56a865f7f3
liva <ebammo20@gmail.com>
2023-02-03 21:03:05 +0330

    Reusing Decorators

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 """
 Thanks to: https://realpython.com/primer-on-python-decorators/
 
-Syntactic Sugar!
-So, @my_decorator is just an easier way of saying say_whee = my_decorator(say_whee). It’s how you
-apply a decorator to a function. """
+Reusing Decorators
+"""
+from decorators import do_twice
 
 
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-
-    return wrapper
-
-
-@my_decorator
+@do_twice
 def say_whee():
     print("Whee!")
 
