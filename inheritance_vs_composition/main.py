7a9c80673e746f46a377a01e80f0d34a1daee9ae
liva <ebammo20@gmail.com>
2023-02-09 22:13:54 +0330

    Duck typing

--- inheritance_vs_composition/main.py
+++ inheritance_vs_composition/main.py
@@ -1,29 +1,33 @@
 """
-Abstract Base Classes in Python
-Inheritance and composition are two major concepts in object-oriented programming that model the relationship between
-two classes. They drive the design of an application and determine how the application should evolve as new features are
- added
-or requirements change.
+Implementation Inheritance vs Interface Inheritance
+When you derive one class from another, the derived class inherits both:
 
-Both of them enable code reuse, but they do it in different ways.
+The base class interface: The derived class inherits all the methods, properties, and attributes of the base class.
 
-Inheritance models what is called an is a relationship. This means that when you have a Derived class that inherits
-from a Base class, you created a relationship where Derived is a specialized version of Base.
-Let’s say you have a base class Animal and you derive from it to create a Horse class. The inheritance relationship
-states that a Horse is an Animal. This means that Horse inherits the interface and implementation of Animal,
-and Horse objects can be used to replace Animal objects in the application.
+The base class implementation: The derived class inherits the code that implements the class interface.
 
-What’s Composition?
-Composition is a concept that models a has a relationship. It enables creating complex types by combining objects
-of other types. This means that a class Composite can contain an object of another class Component.
-This relationship means that a Composite has a Component.
+Most of the time, you’ll want to inherit the implementation of a class, but you will want to implement multiple
+interfaces, so your objects can be used in different situations.
 
-Note: Classes that contain objects of other classes are usually referred to as composites, where classes that are used
-to create more complex types are referred to as components.
+In Python, you don’t have to explicitly declare an interface. Any object that implements the desired interface can be
+ used in place of another object. This is known as duck typing. Duck typing is usually explained as “if it behaves
+ like a duck, then it’s a duck.”
 
+ Since you don’t have to derive from a specific class for your objects to be reusable by the program, you may be
+  asking why you should use inheritance instead of just implementing the desired interface. The following rules
+   may help you:
+
+Use inheritance to reuse an implementation: Your derived classes should leverage most of their base class
+implementation. They must also model an is a relationship. A Customer class might also have an id and a name,
+but a Customer is not an Employee, so you should not use inheritance.
+
+Implement an interface to be reused: When you want your class to be reused by a specific part of your application,
+ you implement the required interface in your class, but you don’t need to provide a base class,
+ or inherit from another class.
 """
 import hr
 import payroll
+import disgruntled
 
 if __name__ == "__main__":
     print('inheritance vs composition')
@@ -31,12 +35,11 @@ if __name__ == "__main__":
     salary_employee = hr.SalaryEmployee(1, 'John Smith', 1500)
     hourly_employee = hr.HourlyEmployee(2, 'Jane Doe', 40, 15)
     commission_employee = hr.CommissionEmployee(3, 'Kevin Bacon', 1000, 250)
+    disgruntled_employee = disgruntled.DisgruntledEmployee(20000, 'Anonymous')
     payroll_system = payroll.PayrollSystem()
     payroll_system.calculate_payroll([
         salary_employee,
         hourly_employee,
-        commission_employee
+        commission_employee,
+        disgruntled_employee
     ])
-
-    employee = hr.Employee(1, 'Invalid')
-    payroll_system.calculate_payroll([employee])
